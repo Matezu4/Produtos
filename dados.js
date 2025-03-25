722d6044491a1ff86f6bfaadc829e979a34e687a
Matezu4 <132776639+Matezu4@users.noreply.github.com>
2025-03-25 11:38:44 -0300

    Update dados.js

---
+++ dados.js
@@ -0,0 +1,70 @@
+let dados = [
+    {   nome: "Fanta Uva",
+        descricao: "A Fanta Uva é um refrigerante com sabor de uva, conhecido por sua cor roxa vibrante e sabor doce e refrescante. É uma das variações da marca Fanta, que oferece diversos sabores de refrigerantes ao redor do mundo.",
+        link: "https://www.coca-cola.com/br/pt/brands/fanta/produtos",
+        img: "img/fanta-uva.png"
+    },
+    {
+        nome: "Nikito",
+        descricao: "Nikito refere-se a uma linha de biscoitos da marca Vitarella. Esses biscoitos são conhecidos por serem recheados e vêm em diversos sabores.",
+        link: "https://vitarella.com.br/nikito-chocolate-516g/",
+        img: "img/nikito.png"
+
+    },
+    {
+        nome: "Biscoito de polvilho",
+        descricao: "O biscoito de polvilho é um tipo de biscoito salgado ou doce, muito popular no Brasil, feito com polvilho (fécula de mandioca), água, óleo e ovos. Ele possui uma textura leve e crocante, e pode ser encontrado em diversas formas e sabores.",
+        link: "https://panelinha.com.br/receita/biscoito-de-polvilho",
+        img: "img/polvilho.png"
+
+    },
+    {
+        nome:  "Arroz",
+        descricao:  "O arroz é um alimento básico consumido em todo o mundo, e existem diversos tipos, cada um com suas características e usos específicos.",
+        link: "https://saojoaoalimentos.com/beneficios-do-arroz-um-compilado-de-informacoes-que-voce-precisa-saber/",
+        img: "img/arroz.png"
+
+    },
+    {
+        nome: "Feijão",
+        descricao: "O feijão é um alimento básico e nutritivo, essencial em muitas culturas ao redor do mundo, especialmente na culinária brasileira." ,
+        link: "https://www.zaffari.com.br/mercearia/graos-e-cereais/feijao",
+        img: "img/feijao.png"
+
+    },
+    {
+        nome: "Ovo" ,
+        descricao: "O ovo é um alimento extremamente nutritivo e versátil, consumido em todo o mundo. É uma excelente fonte de proteínas, vitaminas e minerais, e pode ser preparado de diversas maneiras." ,
+        link: "https://www.tuasaude.com/informacao-nutricional-do-ovo/",
+        img: "img/ovo.png"
+
+    },
+    {
+        nome: "Alho",
+        descricao: "O alho (Allium sativum) é um tempero essencial na culinária mundial, conhecido por seu sabor forte e aroma característico. Além de seu uso culinário, o alho possui propriedades medicinais que o tornam um alimento valioso para a saúde." ,
+        link: "https://blog.amorsaude.com.br/beneficios-do-alho/",
+        img: "img/alho.png"
+
+    },
+    {
+        nome: "Cebola",
+        descricao: "A cebola (Allium cepa) é um vegetal bulboso amplamente utilizado na culinária mundial, conhecido por seu sabor pungente e aroma característico. Além de seu uso culinário, a cebola possui propriedades medicinais que a tornam um alimento valioso para a saúde.",
+        link: "https://www.ecycle.com.br/cebola/",
+        img: "img/cebola.png"
+
+    },
+    {
+        nome: "Biscoito Cream Crack" ,
+        descricao: "O biscoito cream cracker é um tipo de biscoito salgado, fino e crocante, feito à base de farinha de trigo. É conhecido por sua textura leve e sabor neutro, o que o torna um acompanhamento versátil para diversos alimentos." ,
+        link: "https://pt.wikipedia.org/wiki/Cream_cracker",
+        img: "img/crack.png"
+
+    },
+    {
+        nome: "Molho de tomate" ,
+        descricao: "O molho de tomate é um ingrediente essencial na culinária mundial, utilizado em uma variedade de pratos, desde massas até pizzas e ensopados. Ele é feito a partir de tomates cozidos e temperados, resultando em um molho saboroso e versátil." ,
+        link: "https://www.socilink.com/noticias/diferenca-entre-molho-concentrado-polpa-e-passata-de-tomate/",
+        img: "img/molho.png"
+
+    }
+];
